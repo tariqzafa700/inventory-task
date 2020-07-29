1e882799a6e2fcb5db5121c614dc63ee1eb100bb
tariq <tariq.zafar007@live.com>
2020-07-28 21:39:24 -0700

    api key for security

--- src/main/java/com/gildedroses/inventory/operations/security/WebSecurityConfig.java
+++ src/main/java/com/gildedroses/inventory/operations/security/WebSecurityConfig.java
@@ -3,34 +3,66 @@ package com.gildedroses.inventory.operations.security;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
+
+import com.gildedroses.inventory.operations.security.filter.ApiKeySecurityFilter;
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 
+	@Value("${app.http.auth-token-header-name}")
+    private String principalRequestHeader;
+
+    @Value("${app.http.auth-token-value}")
+    private String principalRequestValue;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception{
+    	ApiKeySecurityFilter filter = new ApiKeySecurityFilter(principalRequestHeader);
+        filter.setAuthenticationManager(new AuthenticationManager() {
+			
+            @Override
+            public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+                String principal = (String) authentication.getPrincipal();
+                System.out.println("here " + principal + " saved " + principalRequestValue);
+                if (!principalRequestValue.equals(principal)) {
+                    throw new BadCredentialsException("The API key is invalid or absent.");
+                }
+                authentication.setAuthenticated(true);
+                return authentication;
+            }
+        });
+        
     	// Can enable other mechanisms for csrf like hashed csrf cookie and a request header in each request.
-        http.cors().and().csrf().disable()
-		  .authorizeRequests(authorize -> {
-			try {
-				authorize
-				.mvcMatchers("/buy/**").authenticated()
-				.and()
-				.httpBasic();
-			} catch (Exception e) {
-				System.err.println("exception " + e.getMessage());
-			}
-		});
+        http.addFilterBefore(filter, AbstractPreAuthenticatedProcessingFilter.class)
+		    .authorizeRequests(authorize -> {
+				try {
+					authorize
+					.mvcMatchers("/buy/**")
+					.authenticated()
+					.and()
+					.httpBasic();
+				} catch (Exception e) {
+					throw new AuthenticationCredentialsNotFoundException("Auth failed");
+				}
+		    }).cors().and().csrf().disable().logout().deleteCookies("JSESSIONID");
     }
 
     @Bean
---
+++ src/main/java/com/gildedroses/inventory/operations/security/filter/ApiKeySecurityFilter.java
@@ -0,0 +1,31 @@
+package com.gildedroses.inventory.operations.security.filter;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
+
+public class ApiKeySecurityFilter extends AbstractPreAuthenticatedProcessingFilter {
+
+    private String principalRequestHeader;
+
+    public ApiKeySecurityFilter(String principalRequestHeader) {
+        this.principalRequestHeader = principalRequestHeader;
+    }
+
+    @Override
+    protected Object getPreAuthenticatedPrincipal(HttpServletRequest request) {
+        return request.getHeader(principalRequestHeader);
+    }
+
+    @Override
+    protected Object getPreAuthenticatedCredentials(HttpServletRequest request) {
+        return "N/A";
+    }
+}
+
+
